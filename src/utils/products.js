a3042e9ef73a775bb2b8bed24e046bec9d209624
Racharla saikrishna <115521007+saikrishnaracharla@users.noreply.github.com>
2024-11-14 19:01:50 +0530

    chasjh

--- src/utils/products.js
+++ src/utils/products.js
@@ -5,7 +5,7 @@ import productImg03 from "../Images/double-sofa-03.png";
 import productImg04 from "../Images/single-sofa-01.jpg";
 import productImg05 from "../Images/single-sofa-02.jpg";
 import productImg06 from "../Images/single-sofa-03.jpg";
-import productImg007 from "../Images/single-sofa-04.png";
+// import productImg007 from "../Images/single-sofa-04.png";
 
 import productImg07 from "../Images/arm-chair-01.jpg";
 import productImg08 from "../Images/arm-chair-02.jpg";
@@ -20,15 +20,15 @@ import phone05 from "../Images/phone-05.jpg";
 import phone06 from "../Images/phone-06.jpg";
 import phone08 from "../Images/phone-08.png";
 
-import watch01 from "../Images/watch-01.jpg";
-import watch02 from "../Images/watch-02.jpg";
-import watch03 from "../Images/watch-03.jpg";
-import watch04 from "../Images/watch-04.jpg";
+// import watch01 from "../Images/watch-01.jpg";
+// import watch02 from "../Images/watch-02.jpg";
+// import watch03 from "../Images/watch-03.jpg";
+// import watch04 from "../Images/watch-04.jpg";
 
 import wireless01 from "../Images/wireless-01.png";
-import wireless02 from "../Images/wireless-02.png";
-import wireless03 from "../Images/wireless-03.png";
-import wireless04 from "../Images/wireless-04.png";
+// import wireless02 from "../Images/wireless-02.png";
+// import wireless03 from "../Images/wireless-03.png";
+// import wireless04 from "../Images/wireless-04.png";
 
 import sofaSlide from "../Images/hero-img.png";
 import watchSlide from "../Images/watch-07.png";
@@ -278,5 +278,5 @@ export const products = [
     price: 899,
   
   },
+];
 
-  
